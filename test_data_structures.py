f83545f64a099ffda2fa57bdbecbfcaa70e70d2c
AngrySigma <illariov1809@gmail.com>
2022-09-13 00:35:56 +0300

    Join test

--- test_data_structures.py
+++ test_data_structures.py
@@ -4,6 +4,11 @@ import pytest
 
 
 @pytest.fixture
+def test_tuple():
+    return 1, 1000, -1
+
+
+@pytest.fixture
 def inf():
     return float('inf')
 
@@ -11,6 +16,35 @@ def inf():
 @pytest.fixture
 def nan():
     return float('nan')
+
+
+@pytest.mark.parametrize('test_index, expected',
+                         [(0, 1), (2, -1), (-1, -1), (-3, 1), (True, 1000)])
+def test_tuple_indices(test_index, expected, test_tuple):
+    assert test_tuple[test_index] == expected
+
+
+@pytest.mark.parametrize('test_index, expected_error',
+                         [(3, IndexError), (-4, IndexError), (100, IndexError),
+                          (-100, IndexError), ('1', TypeError),
+                          (None, TypeError), ((1,), TypeError)])
+def test_tuple_index_errors(test_index, expected_error, test_tuple):
+    try:
+        assert test_tuple[test_index]
+        pytest.fail()
+    except Exception as e:
+        assert isinstance(e, expected_error)
+
+
+@pytest.mark.parametrize('test_input',
+                         [1, '1', 'a', None, 'None', True, 'True', [0, 1],
+                          '[0, 1]', {0, 1}, {0: 1, 2: 3},
+                          object])
+def test_tuple_data_types(test_input):
+    test_tuple = tuple([1, '1', 'a', None, 'None', True, 'True', [0, 1],
+                        '[0, 1]', {0, 1}, {0: 1, 2: 3},
+                        object])
+    assert test_input in test_tuple
 
 
 @pytest.mark.parametrize('test_input, result',
@@ -43,5 +77,5 @@ def test_float_int(test_input, result):
                                         'isnan(inf - nan)',
                                         'isnan(inf + nan)'
                                         ])
-def test_infinity(test_input, inf, nan):
+def test_float_infinity(test_input, inf, nan):
     assert eval(test_input)
